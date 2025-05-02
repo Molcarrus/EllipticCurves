ccabcf5c493a18b448adf6367e98f0c0bf81ebc3
Molcarrus <lenovolaptop.1202023@gmail.com>
2025-05-02 14:53:58 +0530

    env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem="windows")]
 #![allow(rustdoc::missing_crate_level_docs)]
 
+use eframe::egui::{
+    self,
+    Color32,
+    Event,
+    Slider,
+    Vec2
+};
+
+use egui_plot::{
+    Legend,
+    Line,
+    Plot, 
+    PlotPoint,
+    PlotPoints,
+};
 
 fn main() {
     println!("Hello, world!");
